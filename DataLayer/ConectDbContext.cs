0e6355d17f172a6bf38fc95da971bb04b94b708a
alijafari0572 <alijafari0572@gmail.com>
2022-10-14 17:27:49 +0330

    1

---
+++ DataLayer/ConectDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Entities;
+namespace DataLayer
+{
+    public class ConectDbContext:DbContext
+    {
+        public ConectDbContext( DbContextOptions options)
+        : base(options)
+        {
+            
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
