a741e760f03c3173d7b9ab574d0f77addb8abec2
alijafari0572 <alijafari0572@gmail.com>
2022-10-14 16:45:54 +0330

    Initial commit

---
+++ ContactPage_Elitecrescent/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ContactPage_Elitecrescent.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
